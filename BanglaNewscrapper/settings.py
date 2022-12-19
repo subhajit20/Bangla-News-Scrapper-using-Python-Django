c704910bcb1d3900bcb0374a99b47665468a6ba8
Subhajit Ghosh <subhajitstd07@gmail.com>
2022-12-19 22:36:38 +0530

    Add cors

--- BanglaNewscrapper/settings.py
+++ BanglaNewscrapper/settings.py
@@ -79,6 +79,7 @@ CORS_ALLOW_METHODS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:5500",
+    "https://subhajit20.github.io"
 ]
 
 ROOT_URLCONF = 'BanglaNewscrapper.urls'
